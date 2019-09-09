041596583d33e6b0bdc2064162c22270a107ce2c
Michelle Polinsky <michelle.polinsky@gmail.com>
2019-09-09 16:41:48 -0400

    Success

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace intro_to_dotnet
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var min = 1;
+      var max = 100;
+      var lower = min;
+      var upper = max;
+      var guess = 50;
+      var guessing = true;
+      Console.WriteLine("Pick a number between 1 and 100");
+
+      while (guessing == true)
+      {
+        Console.WriteLine($"Is Your Number,{guess}");
+
+        var input = Console.ReadLine();
+
+        if (input == "yes")
+        {
+          guessing = false;
+        }
+        else
+        {
+          Console.WriteLine("Higher or Lower?");
+          var high = Console.ReadLine();
+          if (high == "higher")
+          {
+            min = guess;
+            guess = (min + max) / 2;
+          }
+          else
+          {
+            max = guess;
+            guess = (min + max) / 2;
+          }
+        }
+
+        {
+
+
+        }
+
+
+
+
+      }
+      Console.WriteLine($"Congrats! Your number is {guess}");
+    }
+  }
+}
+
+
